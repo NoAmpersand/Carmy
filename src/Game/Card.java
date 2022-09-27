b71971cf530339da890cca1acdb2e8d3f1924b5e
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2022-09-27 10:06:38 +0200

    Update Card.java

--- src/Game/Card.java
+++ src/Game/Card.java
@@ -1,3 +1,43 @@
 public class Card{
+    public enum Couleur{ 
+        COEUR,
+        CARRE,
+        TREFLE,
+        PIQUE;
+    } 
+    public enum Valeur{
+        AS,
+        DEUX,
+        TROIS,
+        QUATRE,
+        CINQ,
+        SIX,
+        SEPT,
+        HUIT,
+        NEUF,
+        DIX,
+        VALET,
+        REINE,
+        ROI;
+    }
+    public final Valeur valeur;
+    public final Couleur couleur;
+
+    public Card(Valeur valeur, Couleur couleur){
+        this.valeur = valeur;
+        this.couleur = couleur;
+    }
+
+    public Valeur getValeur(){
+        return valeur;
+    }
+
+    public Couleur getCouleur(){
+        return couleur;
+    }
+
+    public String affichage(){
+        return valeur + " de " + couleur;
+    }
     
 }
