4ca51f0c314970473dd33391d9dcf0903e7880ee
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2022-09-27 10:09:10 +0200

    Update Player.java

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,3 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Player {
-    
+    private String nom;
+    private List<Card> hand = new ArrayList<Card>();
+
+    //constructeur classe player
+    public Player(String unNom){ 
+        this.nom = unNom; 
+    }
+
+    public void setHand(List<Card> main){
+        this.hand = main;
+        for(Card elem: main)
+        {
+       	    System.out.println (elem.affichage());
+        }
+        System.out.println("-----------------");
+    }
+
+    public void setNom(String nom){
+        this.nom = nom;
+    }
 }
